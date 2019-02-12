143b85525f434ec8d292c0560a29c5f60e7bae38
Nick Perez <nickjperez@gmail.com>
2019-02-12 05:04:43 -0500

    wip: ssl + websockets

--- laravel-echo-server.js
+++ laravel-echo-server.js
@@ -1,37 +1,37 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const env = process.env;
 
-require('laravel-echo-server').run({
-	"authHost": "http://web",
-	"authEndpoint": "/broadcasting/auth",
-	"clients": [],
-	"database": "redis",
-	"databaseConfig": {
-		"redis": {
-            "host": "redis"
-         },
-		"sqlite": {
-			"databasePath": "/database/laravel-echo-server.sqlite"
-		}
-	},
-	"devMode": true,
-	"host": null,
-	"port": "8234",
-	"protocol": "http",
-	"socketio": {},
-	"sslCertPath": "",
-	"sslKeyPath": "",
-	"sslCertChainPath": "",
-	"sslPassphrase": "",
-	"subscribers": {
-		"http": true,
-		"redis": true
-	},
-	"apiOriginAllow": {
-		"allowCors": false,
-		"allowOrigin": "",
-		"allowMethods": "",
-		"allowHeaders": ""
-	}
+require("laravel-echo-server").run({
+  authHost: "http://web",
+  authEndpoint: "/broadcasting/auth",
+  clients: [],
+  database: "redis",
+  databaseConfig: {
+    redis: {
+      host: "redis"
+    },
+    sqlite: {
+      databasePath: "/database/laravel-echo-server.sqlite"
+    }
+  },
+  devMode: true,
+  host: null,
+  port: "8234",
+  protocol: env.APP_ENV === "production" ? "https" : "http",
+  socketio: {},
+  sslCertPath: "keys/cert.pem",
+  sslKeyPath: "keys/key.pem",
+  sslCertChainPath: "",
+  sslPassphrase: "",
+  subscribers: {
+    http: true,
+    redis: true
+  },
+  apiOriginAllow: {
+    allowCors: false,
+    allowOrigin: "",
+    allowMethods: "",
+    allowHeaders: ""
+  }
 });
