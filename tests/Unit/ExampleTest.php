e268df3436d93d96e25fc66da880ab305a3adf87
Nick Perez <nickjperez@gmail.com>
2019-02-18 03:51:03 -0500

    tests, refactor for testability

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -3,9 +3,8 @@
 namespace Tests\Unit;
 
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class ExampleTest extends TestCase
+class Test extends TestCase
 {
     /**
      * A basic test example.
@@ -14,6 +13,46 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $this->assertTrue(true);
+        $mock = $this->getMockForTrait('App\Traits\UpdatesStreamData');
+
+        $mockstats = array_fill(0, 720, [0, 100]);
+        $expectedStats = array_fill(0, 719, [0, 100]);
+        array_push($expectedStats, [1550179875000,300]);
+        
+
+        $cachedStreams = [[
+            'thumbnail' => 'http://test',
+            'url' => 'http://test',
+            'display_name' => 'Test Stream',
+            'name' => 'teststream',
+            'id' => 1,
+            'stats' => $mockstats
+        ]];
+
+        $newStreams = [[
+            'viewers' => 300,
+            'channel' => [
+                '_id' => 1,
+                'name' => 'teststream',
+                'display_name' => 'Test Stream',
+                'url' => 'http://test'
+            ],
+            'preview' => [
+                'medium' => 'http://test'
+            ]
+        ]];
+
+        $expectedStreams = collect([[
+            'thumbnail' => 'http://test',
+            'url' => 'http://test',
+            'display_name' => 'Test Stream',
+            'name' => 'teststream',
+            'id' => 1,
+            'stats' => $expectedStats
+        ]]);
+
+        $timestamp = 1550179875;
+
+        $this->assertEquals($mock->updateStreamData($newStreams, $cachedStreams, $timestamp), $expectedStreams);
     }
 }
